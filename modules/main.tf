e9c249124f923b4e5bc3815a6cf5c278bdd23530
lu.cesarano <9QbNmbGjQp5UED9>
2021-05-06 22:51:57 +0200

    Refactoring

--- modules/main.tf
+++ modules/main.tf
@@ -13,13 +13,13 @@ provider "docker" {
 }
 
 module "mysql" {
-  source                = "docker_containers/mysql"
+  source                = "./docker_containers/mysql"
   workspace             = var.workspace
   mysql_external_port_1 = ""
   mysql_internal_port_1 = ""
 }
 
 #module "portainer" {
-#  source    = "./modules/docker_containers/portainer"
+#  source    = "./docker_containers/portainer"
 #  workspace = var.workspace
 #}
