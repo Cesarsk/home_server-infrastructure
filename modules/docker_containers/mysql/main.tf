e755eeaa0792738aade7ede7ace1afd2bc69e3e1
lu.cesarano <9QbNmbGjQp5UED9>
2021-05-12 22:00:05 +0200

    Updated main pipeline

--- modules/docker_containers/mysql/main.tf
+++ modules/docker_containers/mysql/main.tf
@@ -12,8 +12,9 @@ data "docker_registry_image" "mysql_registry_image" {
 }
 
 resource "docker_image" "mysql_image" {
-  name         = data.docker_registry_image.mysql_registry_image.name
-  keep_locally = true
+  name          = data.docker_registry_image.mysql_registry_image.name
+  keep_locally  = true
+  pull_triggers = [data.docker_registry_image.mysql_registry_image.sha256_digest]
 }
 
 # create db container
